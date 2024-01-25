517179e415b2f023bca391112a47dc6aa17f42d2
whale <lje1203@yonsei.ac.kr>
2024-01-26 01:34:31 +0900

    part1: print out data from mongodb

--- server.js
+++ server.js
@@ -32,10 +32,8 @@ app.get("/news", (요청, 응답) => {
   // 응답.send("오늘 비옴");
 });
 
-app.get("/shop", (요청, 응답) => {
-  응답.send("쇼핑 페이지임");
-});
-
-app.get("/about", (요청, 응답) => {
-  응답.sendFile(__dirname + "/about.html");
+app.get("/list", async (요청, 응답) => {
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+  응답.send(result[0].title);
 });
